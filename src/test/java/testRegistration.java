d7d01c6d17a1e34b19125ec99c439a1280273e34
lockbird <lockedbird96@gmail.com>
2023-07-21 22:03:17 +0300

    added new class

---
+++ src/test/java/testRegistration.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+public class testRegistration {
+    @Test
+    void validRegistrationTest () {
+        System.out.println("This, maybe, test in valid registration");
+    }
+}
