8d2cff3a2bfbc54ff60dadbd025e826eb455ad2c
lockbird <lockedbird96@gmail.com>
2023-07-21 23:34:31 +0300

    added new class

--- src/test/java/testRegistration.java
+++ src/test/java/testRegistration.java
@@ -1,8 +1,15 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class testRegistration {
     @Test
     void validRegistrationTest () {
-        System.out.println("This, maybe, test in valid registration");
+        open("https://volchok.ru/");
+        $("#Layer_1").click();
+        Configuration.holdBrowserOpen = true;
     }
 }
