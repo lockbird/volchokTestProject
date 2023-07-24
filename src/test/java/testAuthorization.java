e3c4fd4511f527ec648b764275030abda0e92fdf
lockbird <lockedbird96@gmail.com>
2023-07-25 01:50:05 +0300

    added new class

--- src/test/java/testAuthorization.java
+++ src/test/java/testAuthorization.java
@@ -1,14 +1,27 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class testAuthorization {
+
+    @BeforeEach
+    void openPage() {
+        open("https://concepture.club");
+    }
+
+    @AfterEach
+    void closeWindow() {
+        closeWebDriver();
+    }
+
     @Test
     void validAuthorizationTest () {
         Configuration.timeout = 5000;
-        open("http://concepture.club/");
         $("[class='fa fa-user-o']").click();
         $("[class='fa fa-sign-in']").click();
         $("#frontendauthform-indentity").setValue("lockedbird96@gmail.com");
@@ -21,7 +34,6 @@ public class testAuthorization {
     @Test
     void invalidAuthorizationTest () {
         Configuration.timeout = 5000;
-        open("http://concepture.club/");
         $("[class='fa fa-user-o']").click();
         $("[class='fa fa-sign-in']").click();
         $("#frontendauthform-indentity").setValue("blabla@gmail.com");
