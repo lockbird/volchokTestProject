11b0aaa67dea06d2f33725d2169bba3136f781a2
lockbird <lockedbird96@gmail.com>
2023-07-25 00:16:31 +0300

    added new class

--- src/test/java/testAuthorization.java
+++ src/test/java/testAuthorization.java
@@ -1,14 +1,32 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class testAuthorization {
     @Test
     void validAuthorizationTest () {
-        Configuration.holdBrowserOpen = true;
         Configuration.timeout = 5000;
         open("http://concepture.club/");
         $("[class='fa fa-user-o']").click();
         $("[class='fa fa-sign-in']").click();
+        $("#frontendauthform-indentity").setValue("lockedbird96@gmail.com");
+        $("#frontendauthform-validation").setValue("Cr2bSf9pa9zHhB7");
+        $("[name=signup-button]").click();
+        $("[class='fa fa-user-o']").click();
+        $("[class='fa fa-sign-out fa-fw']").shouldBe(visible);
+    }
+
+    @Test
+    void invalidAuthorizationTest () {
+        Configuration.timeout = 5000;
+        open("http://concepture.club/");
+        $("[class='fa fa-user-o']").click();
+        $("[class='fa fa-sign-in']").click();
+        $("#frontendauthform-indentity").setValue("blabla@gmail.com");
+        $("#frontendauthform-validation").setValue("qwerty");
+        $("[name=signup-button]").click();
+        $("#login-form").shouldHave(text("Неправильный логин или пароль."));
     }
 }
